a5fd6a667f51e6fb55f264ef017507786e853be3
Keerthi Ashok <keerthiashok@macbookpro.lan>
2026-01-14 14:06:04 -0500

    Updated to handle invalid data

--- tests/orderValidation.spec.js
+++ tests/orderValidation.spec.js
@@ -6,8 +6,7 @@ const fs = require('fs');
 const axios = require("axios");
 const access_token_path=require("./config.json");
 const ACCESS_TOKEN =access_token_path.access_token;
-// Replace with the order ID to check
-const ORDER_ID = "YOUR_ORDER_ID";
+
 
 async function validatePayPalOrder(orderId) {
   try {
@@ -67,46 +66,55 @@ const createOrderResponse=await request.post("https://api-m.sandbox.paypal.com/v
   }
 
 
-    test.describe('Order flow', () => {
-  let orderResponse;
-  let orderResponseStatus;
-  const workbook = XLSX.readFile('./tests/orderInput.csv');
+
+//Order Creation
+test.describe('Order flow', () => {
+
+    let orderResponse;
+    const workbook = XLSX.readFile('./tests/orderInput.csv');
     const sheet = workbook.Sheets[workbook.SheetNames[0]];
     const orders = XLSX.utils.sheet_to_json(sheet);
 
     const finalRequests =  dataCreation.createDynamicRequestsFromJson(orders);
+    console.log("finalRequests.length()", finalRequests.length);
+    const orderIds = new Array([finalRequests.length]);
+    const orderResponseStatus = new Array([finalRequests.length]);
+    const apiResponsesMap = new Map();
+    const apiResponsesStatusMap = new Map();
 
-    test('Create order', async({ request })=> {
-  for (const [index, data] of finalRequests.entries()) {
-
-        await allure.step(`Creating order for ID: ${data.id}`, async () => {
-  const orderString = JSON.stringify(data, null, 2);
-  console.log("Request body",orderString);
-  let token=access_token_path.access_token;
-  const responseData=await sendRequestOrder(request,token,orderString);
-  orderResponse=await responseData.json();
-  orderResponseStatus=await responseData.status();
-  console.log("responseData",await responseData.json());
-
-expect.soft(
-  responseData.status() === 200 || responseData.status() === 201,
-  'Status should be 200 or 201').toBeTruthy();
+  test('Create order successfully and Validate the response', async({ request })=> {
+    for (const [index, data] of finalRequests.entries()) {
+    await allure.step(`Creating order for ID: ${index}`, async () => {
+    const orderString = JSON.stringify(data, null, 2);
+    console.log("Request body",orderString);
+    let token=access_token_path.access_token;
+    const responseData=await sendRequestOrder(request,token,orderString);
+    orderResponse=await responseData.json();
+    console.log("responseData",await responseData.json());
+    apiResponsesStatusMap.set(index,responseData.status());
+    expect.soft(
+        responseData.status() === 200 || responseData.status() === 201 || responseData.status() === 422,
+        'Status should be valid 200 or 201 or 422').toBeTruthy();
 
       if(responseData.status()===200 || responseData.status() === 201) {
         if (orderResponse.id !=="") {
-      console.log('Order created with status code', responseData.status());
-      //console.log('Order created with response ', responseData.json());
-          await allure.attachment("Response", responseData.json(), "application/json");
-
-      //console.log("Reference_ID", orderString.purchase_units[0].reference_id);
-      console.log("Order ID", orderResponse.id);
-     }
+            console.log('Order created with status code', responseData.status());
+            await allure.attachment("Response", JSON.stringify(orderResponse, null, 2), "application/json");
+            //orderIds.push(orderResponse.id);
+            //responseJson=await responseData.json();
+            apiResponsesMap.set(index,orderResponse);
+        }
+      }
+  else if(responseData.status()===422){
+        console.log('Order created with status code', responseData.status());
+        console.log('Order created with response ', orderResponse);
+        //responseJson=await responseData.json();
+        await allure.attachment("Response", JSON.stringify(orderResponse, null, 2), "application/json");
+        apiResponsesMap.set(index,orderResponse);
   }
   else{
-        console.log('Order created with status code', responseData.status());
-                console.log('Order created with response ', responseData.json());
-
-        //console.log("Reference_ID", orderString.reference_id);
+   console.log('Order created with status code', responseData.status());
+   console.log('Order created with response ', orderResponse);
   }
   });
 }
@@ -115,20 +123,50 @@ expect.soft(
 
   test('Order creation - Validate the orderID', async()=>
   {
-      if(orderResponseStatus===200 || orderResponseStatus === 201)
-{
-  if (orderResponse.id !=="" || !orderResponse.id) {
-    expect.soft(orderResponse.id, 'Order ID must exist').toBeTruthy();
-  }
-    console.log("Valid Order Id", orderResponse.id);
-}
-else{
-    console.log("Response status", orderResponseStatus);
+    for (const [key, order] of apiResponsesMap.entries()) {
+        const order=await apiResponsesMap.get(key);
+        //console.log("Order from  Validate the orderID",order);
+        await allure.step(`Validate order ID:${key} ${order.id}`, async () => {
+        const responseStatus=apiResponsesStatusMap.get(key);
+            if(responseStatus===200 || responseStatus === 201) {
+              if (order.id !=="" || !order.id) {
+                    expect.soft(order.id, 'Order ID valid').toBeTruthy();
+                    //await allure.attachment("Order ID", JSON.stringify(order.id, null, 2), "application/json");
+              }
+            console.log("OrderID Not empty  ",order.id);
 
-}
-       });
+            }
+        });
+    }
+  });
 
+ test('Validate that every link contains the corresponding ID', async()=>
+ {
+
+    for (const [key, order] of apiResponsesMap.entries()) {
+       await allure.step(`Validate order ID:${key} ${order.id}`, async () => {
+       const responseStatus=apiResponsesStatusMap.get(key);
+    if(responseStatus===200 || responseStatus === 201) {
+    console.log(`ID in Map: ${key} | Json Response: `);
+    const responseValue=apiResponsesMap.get(key);
+    //console.log(responseValue);
+    responseValue.links.forEach(link => {
+        expect.soft(link.href,
+          `Link href validation for map key ${key}`
+        ).toContain(responseValue.id);
+        //await allure.attachment("", JSON.stringify(order.id, null, 2), "application/json");
+      });
+      }
+      });
+      }
     });
+
+
+    //expect(apiResponsesMap.get(key).links.href).toContain(orderResponse.id);
+
+    //console.log(`Link ${index + 1} (${link.rel}) validated for ID: ${orderId}`);
+  });
+
 
 
 
